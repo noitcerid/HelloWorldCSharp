e489ca5f4e5049bd4ec509d957f692620c910a59
Chris Potter <christhepcguy@gmail.com>
2018-10-07 10:29:43 -0400

    Updated Console Output

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorldCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello TeamCity World!");
         }
     }
 }
